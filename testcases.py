1ae2a227b45234711e81b79d52b7315814ad2307
SwetaSriv <sweta.sriv23@gmail.com>
2023-12-02 18:10:52 +0530

    Update testcases.py

--- testcases.py
+++ testcases.py
@@ -17,42 +17,40 @@ def test_post_root():
     assert response.status_code == 200    
     assert response.get_json()['op'] == "Hello, World POST "+suffix
 
-def test_load_models_route(self):
-        response = self.app.get('/load_models')
-        data = json.loads(response.get_data(as_text=True))
+def test_load_models_route():
+    response = self.app.get('/load_models')
+    data = json.loads(response.get_data(as_text=True))
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('status', data)
-        self.assertEqual(data['status'], 'Models loaded successfully')
-
+    self.assertEqual(response.status_code, 200)
+    self.assertIn('status', data)
+    self.assertEqual(data['status'], 'Models loaded successfully')
 
 def test_predict_route_svm(self):
-        payload = {"suffix": "test_suffix"}
-        response = self.app.post('/predict/svm', json=payload)
-        data = json.loads(response.get_data(as_text=True))
+    payload = {"suffix": "test_suffix"}
+    response = self.app.post('/predict/svm', json=payload)
+    data = json.loads(response.get_data(as_text=True))
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('op', data)
-        self.assertIn('Prediction for svm with suffix', data['op'])
+    self.assertEqual(response.status_code, 200)
+    self.assertIn('op', data)
+    self.assertIn('Prediction for svm with suffix', data['op'])
 
-    def test_predict_route_lr(self):
-        payload = {"suffix": "test_suffix"}
-        response = self.app.post('/predict/lr', json=payload)
-        data = json.loads(response.get_data(as_text=True))
+def test_predict_route_lr(self):
+    payload = {"suffix": "test_suffix"}
+    response = self.app.post('/predict/lr', json=payload)
+    data = json.loads(response.get_data(as_text=True))
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('op', data)
-        self.assertIn('Prediction for lr with suffix', data['op'])
+    self.assertEqual(response.status_code, 200)
+    self.assertIn('op', data)
+    self.assertIn('Prediction for lr with suffix', data['op'])
 
-    def test_predict_route_tree(self):
-        payload = {"suffix": "test_suffix"}
-        response = self.app.post('/predict/tree', json=payload)
-        data = json.loads(response.get_data(as_text=True))
+def test_predict_route_tree(self):
+    payload = {"suffix": "test_suffix"}
+    response = self.app.post('/predict/tree', json=payload)
+    data = json.loads(response.get_data(as_text=True))
 
-        self.assertEqual(response.status_code, 200)
-        self.assertIn('op', data)
-        self.assertIn('Prediction for tree with suffix', data['op'])
-
+    self.assertEqual(response.status_code, 200)
+    self.assertIn('op', data)
+    self.assertIn('Prediction for tree with suffix', data['op'])
 
 
 
