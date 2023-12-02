534c9823311ba560e303ef4564397d75bb5d54b9
SwetaSriv <sweta.sriv23@gmail.com>
2023-12-02 18:52:28 +0530

    Update testcases.py Q3

--- testcases.py
+++ testcases.py
@@ -4,8 +4,6 @@ from sklearn.datasets import fetch_openml
 import numpy as np
 import random
 
-  
-
 def test_get_root():
     response = app.test_client().get("/")
     assert response.status_code == 200
@@ -18,41 +16,45 @@ def test_post_root():
     assert response.get_json()['op'] == "Hello, World POST "+suffix
 
 def test_load_models_route():
-    response = self.app.get('/load_models')
+    response = app.test_client().get('/load_models')
     data = json.loads(response.get_data(as_text=True))
 
-    self.assertEqual(response.status_code, 200)
-    self.assertIn('status', data)
-    self.assertEqual(data['status'], 'Models loaded successfully')
+    assert response.status_code == 200
+    assert 'status' in data
+    assert data['status'] == 'Models loaded successfully'
 
-def test_predict_route_svm(self):
+def test_predict_route_svm():
     payload = {"suffix": "test_suffix"}
-    response = self.app.post('/predict/svm', json=payload)
+    response = app.test_client().post('/predict/svm', json=payload)
     data = json.loads(response.get_data(as_text=True))
 
-    self.assertEqual(response.status_code, 200)
-    self.assertIn('op', data)
-    self.assertIn('Prediction for svm with suffix', data['op'])
+    assert response.status_code == 200
+    assert 'op' in data
+    assert 'Prediction for svm with suffix' in data['op']
 
-def test_predict_route_lr(self):
+def test_predict_route_lr():
     payload = {"suffix": "test_suffix"}
-    response = self.app.post('/predict/lr', json=payload)
+    response = app.test_client().post('/predict/lr', json=payload)
     data = json.loads(response.get_data(as_text=True))
 
-    self.assertEqual(response.status_code, 200)
-    self.assertIn('op', data)
-    self.assertIn('Prediction for lr with suffix', data['op'])
+    assert response.status_code == 200
+    assert 'op' in data
+    assert 'Prediction for lr with suffix' in data['op']
 
-def test_predict_route_tree(self):
+def test_predict_route_tree():
     payload = {"suffix": "test_suffix"}
-    response = self.app.post('/predict/tree', json=payload)
+    response = app.test_client().post('/predict/tree', json=payload)
     data = json.loads(response.get_data(as_text=True))
 
-    self.assertEqual(response.status_code, 200)
-    self.assertIn('op', data)
-    self.assertIn('Prediction for tree with suffix', data['op'])
+    assert response.status_code == 200
+    assert 'op' in data
+    assert 'Prediction for tree with suffix' in data['op']
 
+def test_model_type(m22aie204, solver_name):
+    model = load_model(f"{m22aie204}lr{solver_name}.joblib")
+    assert isinstance(model, LogisticRegression), "Loaded model is not a Logistic Regression model"
 
-
-
-        
+def test_solver_name(m22aie204, solver_name):
+    model = load_model(f"{m22aie204}lr{solver_name}.joblib")
+    model_solver = model.get_params()['solver']
+    assert model_solver == solver_name, f"Solver in the model ({model_solver}) does not match the solver in the file name ({solver_name})"
